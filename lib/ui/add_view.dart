35c5e60d8913112b31d41920b4221366705e91f1
Parth Cheulkar <pscheulkar12@gmail.com>
2022-04-10 23:27:19 +0530

    first commit

---
+++ lib/ui/add_view.dart
@@ -0,0 +1,73 @@
+import 'package:crypto_wallet/net/flutterfire.dart';
+import 'package:flutter/material.dart';
+
+class AddView extends StatefulWidget {
+  AddView({Key? key}) : super(key: key);
+
+  @override
+  State<AddView> createState() => _AddViewState();
+}
+
+class _AddViewState extends State<AddView> {
+  List<String> coins = [
+    "bitcoin",
+    "tether",
+    "ethereum",
+  ];
+
+  String dropdownValue = "bitcoin";
+  TextEditingController _amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        DropdownButton(
+          value: dropdownValue,
+          onChanged: (value) {
+            setState(() {
+              dropdownValue = value.toString();
+            });
+          },
+          items: coins.map<DropdownMenuItem<String>>((value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value.toString()),
+            );
+          }).toList(),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width / 1.3,
+          child: TextFormField(
+            controller: _amountController,
+            decoration: InputDecoration(
+              labelText: "Coin Amount",
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Container(
+            width: MediaQuery.of(context).size.width / 1.4,
+            height: 40,
+            
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              color: Colors.amber[600],
+              
+            ),
+            child: MaterialButton(
+              
+              onPressed: () async {
+                await addCoin(dropdownValue, _amountController.text);
+                Navigator.of(context).pop();
+              },
+              child: Text("Add coin"),
+            )),
+      ],
+    ));
+  }
+}
